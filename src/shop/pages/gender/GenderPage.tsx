0f9a0027524f134617ab1c2d28348ec1e1bb6155
DANIEL21NAULA <163338712+DANIEL21NAULA@users.noreply.github.com>
2025-10-06 14:47:03 -0500

    Navegacion Pantallas}

--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,3 +1,31 @@
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { products } from "@/mocks/products.mock";
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
+import { useParams } from "react-router";
+
 export const GenderPage = () => {
-  return <div>GenderPage</div>;
+  const {gender} = useParams();
+  let genderLabel: string;
+  
+  switch (gender) {
+      case 'men':
+        genderLabel = 'Hombres';
+        break;
+      case 'women':
+        genderLabel = "Mujeres";
+        break;
+      default:
+        genderLabel = "Niños";
+        break;
+  }
+
+
+  return(
+    <>
+      <CustomJumbotron title={`Todos los productos ${genderLabel}`} />
+      <ProductsGrid products={products}/>
+      <CustomPagination totalPages={7}/>
+    </>
+  );
 }
